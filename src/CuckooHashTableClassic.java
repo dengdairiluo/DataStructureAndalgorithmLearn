30b2fa2443dbdc0d3d2811455000b81165164c27
dengdairiluo <1020447548@qq.com>
2019-04-05 22:31:51 +0800

    20190404

--- src/CuckooHashTableClassic.java
+++ src/CuckooHashTableClassic.java
@@ -14,9 +14,8 @@ import java.util.HashSet;
 
 
 /**
- * Cuckoo hash table implementation of hash tables.
- *
- * @author Mark Allen Weiss
+ * @author jianglub
+ * @date 2019/4/2
  */
 public class CuckooHashTableClassic<AnyType> {
     private static final double MAX_LOAD = 0.49;
@@ -25,11 +24,10 @@ public class CuckooHashTableClassic<AnyType> {
     private final HashFamily<? super AnyType> hashFunctions;
     private final int numHashFunctions;
     private int rehashes = 0;
-    private AnyType[] array; // The array of elements
+    private AnyType[] array;
     private int currentSize;
     private int subTableSize;
     private int[] subTableStarts;
-
     /**
      * Construct the hash table.
      */
@@ -39,7 +37,6 @@ public class CuckooHashTableClassic<AnyType> {
 
     /**
      * Construct the hash table.
-     *
      * @param size the approximate initial size.
      */
     public CuckooHashTableClassic(HashFamily<? super AnyType> hf, int size) {
@@ -50,7 +47,6 @@ public class CuckooHashTableClassic<AnyType> {
         for (int i = 0; i < numHashFunctions; i++) {
             subTableStarts[i] = i * subTableSize;
         }
-
         allocateArray(subTableSize * numHashFunctions);
         doClear();
     }
@@ -69,7 +65,6 @@ public class CuckooHashTableClassic<AnyType> {
         while (!isPrime(n)) {
             n += 2;
         }
-
         return n;
     }
 
@@ -84,93 +79,15 @@ public class CuckooHashTableClassic<AnyType> {
         if (n == 2 || n == 3) {
             return true;
         }
-
-        if (n == 1 || n % 2 == 0)
+        if (n == 1 || n % 2 == 0) {
             return false;
-
-        for (int i = 3; i * i <= n; i += 2)
-            if (n % i == 0)
+        }
+        for (int i = 3; i * i < n; i += 2) {
+            if (n % i == 0) {
                 return false;
-
+            }
+        }
         return true;
-    }
-
-    // Simple main
-    public static void main(String[] args) {
-        CuckooHashTableClassic<String> H = new CuckooHashTableClassic<>(new StringHashFamily(2));
-
-        java.util.Random r = new java.util.Random();
-        HashSet<String> t = new HashSet<>();
-
-        System.out.println("Checking... (no more output means success)");
-
-
-        long startTime = System.currentTimeMillis();
-
-        final int NUMS = 2000000;
-        final int GAP = 37;
-
-        for (int i = GAP; i != 0; i = (i + GAP) % NUMS)
-            H.insert("" + i);
-        for (int i = GAP; i != 0; i = (i + GAP) % NUMS)
-            if (H.insert("" + i))
-                System.out.println("OOPS!!! " + i);
-        for (int i = 1; i < NUMS; i += 2)
-            H.remove("" + i);
-
-        for (int i = 2; i < NUMS; i += 2)
-            if (!H.contains("" + i))
-                System.out.println("Find fails " + i);
-
-        for (int i = 1; i < NUMS; i += 2) {
-            if (H.contains("" + i))
-                System.out.println("OOPS!!! " + i);
-        }
-
-
-        long endTime = System.currentTimeMillis();
-
-        System.out.println("Elapsed time: " + (endTime - startTime));
-
-        System.out.println("H size is: " + H.size());
-        System.out.println("Array size is: " + H.capacity());
-
-
-
-        /*
-        Scanner scan = null;
-        Set<String> words = new HashSet<>( );
-
-        try
-        {
-             scan = new Scanner( new FileReader( "dict.txt" ) );
-             String oneLine = null;
-             while( scan.hasNextLine( ) )
-             {
-                 words.add( oneLine = scan.nextLine( ) );
-                 H.insert( oneLine );
-             }
-
-             if( words.size( ) != H.size( ) )
-                 System.out.println( "OOPS!!! SIZE MISMATCH " +
-                         words.size( ) + " " + H.size( ) );
-
-             for( String s : words )
-                 if( !H.contains( s ) )
-                     System.out.println( "OOPS!! COULDN'T FIND " + s );
-
-             System.out.println( "H size is: " + H.size( ) );
-             System.out.println( "Array size is: " + H.capacity( ) );
-        }
-        catch( IOException e )
-        {
-        }
-        finally
-        {
-            if( scan != null )
-                scan.close( );
-        }
-        */
     }
 
     /**
@@ -181,22 +98,17 @@ public class CuckooHashTableClassic<AnyType> {
      */
     public boolean insert(AnyType x) {
         final int COUNT_LIMIT = 100;
-
         if (contains(x)) {
             return false;
         }
-
         if (currentSize >= array.length * MAX_LOAD) {
             expand();
         }
-
         for (int i = 0, which = 0; i < COUNT_LIMIT; i++, which++) {
             if (which == numHashFunctions) {
                 which = 0;
             }
-
             int pos = myhash(x, which);
-
             if (array[pos] == null) {
                 array[pos] = x;
                 currentSize++;
@@ -220,12 +132,10 @@ public class CuckooHashTableClassic<AnyType> {
 
     protected int myhash(AnyType x, int which) {
         int hashVal = hashFunctions.hash(x, which);
-
         hashVal %= subTableSize;
         if (hashVal < 0) {
             hashVal += subTableSize;
         }
-
         return hashVal + subTableStarts[which];
     }
 
@@ -235,27 +145,23 @@ public class CuckooHashTableClassic<AnyType> {
 
     private void rehash() {
         //System.out.println( "NEW HASH FUNCTIONS " + array.length );
-        hashFunctions.generateNewFunctions();
+        hashFunctions.generateNewFunctions( );
         rehash(array.length);
     }
 
     private void rehash(int newLength) {
-        //   System.out.println( "REHASH: " + array.length + " " + newLength + " " + currentSize );
-
-        AnyType[] oldArray = array;    // Create a new double-sized, empty table
-
+     //   System.out.println( "REHASH: " + array.length + " " + newLength + " " + currentSize );
+        
+        AnyType [ ] oldArray = array;    // Create a new double-sized, empty table
         if (newLength != array.length) {
             subTableSize = newLength / numHashFunctions;
             for (int i = 0; i < numHashFunctions; i++) {
                 subTableStarts[i] = i * subTableSize;
             }
         }
-
         allocateArray(newLength);
-
         currentSize = 0;
-
-        // Copy table over
+            // Copy table over
         for (AnyType str : oldArray) {
             if (str != null) {
                 insert(str);
@@ -290,10 +196,10 @@ public class CuckooHashTableClassic<AnyType> {
     private int findPos(AnyType x) {
         for (int i = 0; i < numHashFunctions; i++) {
             int pos = myhash(x, i);
-            if (array[pos] != null && array[pos].equals(x))
+            if (array[pos] != null && array[pos].equals(x)) {
                 return pos;
+            }
         }
-
         return -1;
     }
 
@@ -305,12 +211,10 @@ public class CuckooHashTableClassic<AnyType> {
      */
     public boolean remove(AnyType x) {
         int pos = findPos(x);
-
         if (pos != -1) {
             array[pos] = null;
             currentSize--;
         }
-
         return pos != -1;
     }
 
@@ -346,5 +250,5 @@ public class CuckooHashTableClassic<AnyType> {
     private void allocateArray(int arraySize) {
         array = (AnyType[]) new Object[arraySize];
     }
-
 }
+
